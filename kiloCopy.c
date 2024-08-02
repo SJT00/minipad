8d9130415d31181e9348bcc425eb6c462c6f2e24
SJT00 <saadtaj96@gmail.com>
2024-08-02 14:48:02 -0400

    prettier

--- kiloCopy.c
+++ kiloCopy.c
@@ -15,63 +15,77 @@ struct termios orig_termios;
 
 /*** data ***/
 
-void die(const char *s) {
+void die(const char *s)
+{
 	perror(s);
 	exit(1);
 }
 
 /*** terminal ***/
 
-void disableRawMode(void) {
-	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+void disableRawMode(void)
+{
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+		die("tcsetattr");
 }
 
-void enableRawMode(void) {
-	if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+void enableRawMode(void)
+{
+	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+		die("tcgetattr");
 	atexit(disableRawMode);
 	struct termios raw = orig_termios;
-	raw.c_iflag &= ~(ICRNL| IXON| ISTRIP| INPCK| BRKINT);
+	raw.c_iflag &= ~(ICRNL | IXON | ISTRIP | INPCK | BRKINT);
 	raw.c_oflag &= ~(OPOST);
 	raw.c_cflag &= (CS8);
-	raw.c_lflag &= ~(ECHO| ICANON| ISIG| IEXTEN);
+	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
 	raw.c_cc[VMIN] = 0;
 	raw.c_cc[VTIME] = 1;
-	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+		die("tcsetattr");
 }
 
-char editorReadKey(void) {
+char editorReadKey(void)
+{
 	int nread;
 	char c;
-	while ((nread = read(STDIN_FILENO, &c,1)) != 1){
-		if (nread == -1 && errno != EAGAIN) die("read");
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+	{
+		if (nread == -1 && errno != EAGAIN)
+			die("read");
 	}
 	return c;
 }
 
 /*** output ***/
-void editorRefreshScreen(void) {
+void editorRefreshScreen(void)
+{
 	write(STDOUT_FILENO, "\x1b[2J", 4);
 }
 
-void editorProcessKeypress(void) {
+void editorProcessKeypress(void)
+{
 	char c = editorReadKey();
-	
-	switch (c) {
-		case CTRL_KEY('q'):
-			exit(0);
-			break;
+
+	switch (c)
+	{
+	case CTRL_KEY('q'):
+		exit(0);
+		break;
 	}
 }
 
 /*** init ***/
 
-int main(void) {
+int main(void)
+{
 	enableRawMode();
 
-	while (1) {
+	while (1)
+	{
 		editorRefreshScreen();
 		editorProcessKeypress();
 	}
-		
+
 	return 0;
 }
