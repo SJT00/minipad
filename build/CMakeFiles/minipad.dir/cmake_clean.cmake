4c20067c5a06886a3d43c90a85a0101b0263953d
SJT00 <saadtaj96@gmail.com>
2024-08-13 19:46:58 -0400

    setup cmake and make config, plus shell

---
+++ build/CMakeFiles/minipad.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/saadtaj/Desktop/CS Projects/textEditorTutorial/minipad"
+  "/Users/saadtaj/Desktop/CS Projects/textEditorTutorial/minipad.pdb"
+  "CMakeFiles/minipad.dir/main.cpp.o"
+  "CMakeFiles/minipad.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/minipad.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
