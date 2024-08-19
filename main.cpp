f5d04bdb4af0a4aad8da4dd1b9a094667029dde6
SJT00 <saadtaj96@gmail.com>
2024-08-18 23:03:35 -0400

    loading fonts done

--- main.cpp
+++ main.cpp
@@ -4,11 +4,28 @@
 #endif
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
+#include <ft2build.h>
+#include <glm/glm.hpp>
+#include FT_FREETYPE_H
 #include <iostream>
+#include <map>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
+void loadCharacters(FT_Face *face);
+
+const unsigned int scrWidth = 800;
+const unsigned int scrHeight = 600;
+
+struct Character
+{
+    unsigned int TextureID; // ID handle of the glyph texture
+    glm::ivec2 Size;        // Size of glyph
+    glm::ivec2 Bearing;     // Offset from baseline to left/top of glyph
+    unsigned int Advance;   // Offset to advance to next glyph
+};
+
+std::map<char, Character> Characters;
 
 int main()
 {
@@ -16,9 +33,12 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+#endif
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "Minipad", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(scrWidth, scrHeight, "Minipad", NULL, NULL);
+
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW Window" << std::endl;
@@ -33,9 +53,31 @@ int main()
         return -1;
     }
 
-    glViewport(0, 0, 800, 600);
+    glViewport(0, 0, scrWidth, scrHeight);
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    FT_Library ft;
+
+    if (FT_Init_FreeType(&ft))
+    {
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+        return -1;
+    }
+
+    FT_Face face;
+    if (FT_New_Face(ft, "fonts/Futura.ttc", 0, &face))
+    {
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+        return -1;
+    }
+
+    FT_Set_Pixel_Sizes(face, 0, 48);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // disable byte-alignment restriction
+    loadCharacters(&face);
+
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -43,7 +85,7 @@ int main()
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -61,4 +103,53 @@ void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+}
+
+void loadCharacters(FT_Face *face)
+{
+    if (face == nullptr)
+    {
+        std::cout << "ERROR: FT_Face pointer is null" << std::endl;
+        return;
+    }
+    if (*face == nullptr)
+    {
+        std::cout << "ERROR: FT_Face is null" << std::endl;
+        return;
+    }
+    for (unsigned char c = 0; c < 128; c++)
+    {
+        // load character glyph
+        if (FT_Load_Char(*face, c, FT_LOAD_RENDER))
+        {
+            std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+            continue;
+        }
+        // generate texture
+        unsigned int texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RED,
+            (*face)->glyph->bitmap.width,
+            (*face)->glyph->bitmap.rows,
+            0,
+            GL_RED,
+            GL_UNSIGNED_BYTE,
+            (*face)->glyph->bitmap.buffer);
+        // set texture options
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        // now store character for later use
+        Character character = {
+            texture,
+            glm::ivec2((*face)->glyph->bitmap.width, (*face)->glyph->bitmap.rows),
+            glm::ivec2((*face)->glyph->bitmap_left, (*face)->glyph->bitmap_top),
+            (unsigned int)(*face)->glyph->advance.x};
+        Characters.insert(std::pair<char, Character>(c, character));
+    }
 }
