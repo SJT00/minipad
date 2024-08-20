35d7cc2eaab12d7c1bceca5f71ff51d0f2a0495e
SJT00 <saadtaj96@gmail.com>
2024-08-20 03:06:43 -0400

    tmp debbugging flag added

--- build.sh
+++ build.sh
@@ -14,9 +14,19 @@ fi
 # Change to the build directory
 cd "$BUILD_DIR"
 
-# Run CMake and Make
+# Run CMake
 echo "Running CMake..."
-cmake .. -GXCODE
+if [ "$1" == "-db" ]; then
+    echo "Created Xcode project for debugging."
+    cmake -G Xcode ..
+else
+    # Need to add a check for xcode proj here to delete that build and redo
+    echo "Default generator running."
+    cmake ..
 
-echo "Running Make..."
-make
+    # Make binary
+    echo "Running Make..."
+    make
+fi
+
+
