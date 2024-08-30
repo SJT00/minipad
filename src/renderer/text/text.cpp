f632ecba488bdb3ebb79d81001653f9b0f104adf
SJT00 <saadtaj96@gmail.com>
2024-08-30 18:22:46 -0400

    top left is 0,0

--- src/renderer/text/text.cpp
+++ src/renderer/text/text.cpp
@@ -7,7 +7,7 @@ TextRenderer::TextRenderer(unsigned int width, unsigned int height)
     : TextShader("./src/renderer/text/shaders/text_2d.vs", "./src/renderer/text/shaders/text_2d.fs")
 {
     this->TextShader.use();
-    this->TextShader.setMat4("projection", glm::ortho(0.0f, static_cast<float>(width), 0.0f, static_cast<float>(height)));
+    this->TextShader.setMat4("projection", glm::ortho(0.0f, static_cast<float>(width), static_cast<float>(height), 0.0f));
 
     glGenVertexArrays(1, &this->VAO);
     glGenBuffers(1, &this->VBO);
@@ -95,18 +95,18 @@ void TextRenderer::RenderText(
         Character ch = Characters[*c];
 
         float xpos = x + ch.Bearing.x * scale;
-        float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+        float ypos = y + (ch.Size.y - ch.Bearing.y) * scale;
         float w = ch.Size.x * scale;
         float h = ch.Size.y * scale;
 
         float vertices[6][4] = {
-            {xpos, ypos + h, 0.0f, 0.0f},
+            {xpos, ypos - h, 0.0f, 0.0f},
             {xpos, ypos, 0.0f, 1.0f},
             {xpos + w, ypos, 1.0f, 1.0f},
 
-            {xpos, ypos + h, 0.0f, 0.0f},
+            {xpos, ypos - h, 0.0f, 0.0f},
             {xpos + w, ypos, 1.0f, 1.0f},
-            {xpos + w, ypos + h, 1.0f, 0.0f}};
+            {xpos + w, ypos - h, 1.0f, 0.0f}};
 
         glBindTexture(GL_TEXTURE_2D, ch.TextureID);
         glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
