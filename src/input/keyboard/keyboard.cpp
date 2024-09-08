e47206317ae340a0828f5c20304abebd95181e1a
SJT00 <saadtaj96@gmail.com>
2024-09-08 15:14:43 -0400

    cursor wraps now

--- src/input/keyboard/keyboard.cpp
+++ src/input/keyboard/keyboard.cpp
@@ -10,6 +10,8 @@ void KeyboardHandler::CharCallback(GLFWwindow *window, unsigned int codepoint)
     handler->cursorloc[0] += 1;
 }
 
+// write a function to go to last char when moving vertically
+
 void KeyboardHandler::KeyCallback(GLFWwindow *window, int key, int scancode, int action, int mods)
 {
     Editor *handler = (Editor *)glfwGetWindowUserPointer(window);
@@ -17,32 +19,66 @@ void KeyboardHandler::KeyCallback(GLFWwindow *window, int key, int scancode, int
     if (action == GLFW_PRESS || action == GLFW_REPEAT)
     {
         handler->SetCursorActive(true);
-        if (key == GLFW_KEY_RIGHT && handler->cursorloc[0] < handler->textArr[handler->cursorloc[1]].size())
+        if (key == GLFW_KEY_RIGHT)
         {
-            handler->cursorloc[0] += 1;
+            const float lastItm = handler->textArr[handler->cursorloc[1]].size();
+            if (handler->cursorloc[0] < lastItm)
+            {
+                handler->cursorloc[0] += 1;
+            }
+
+            else if (handler->cursorloc[0] == lastItm && handler->textArr[handler->cursorloc[1] + 1].size() > 0)
+            {
+                handler->cursorloc[0] = 0;
+                handler->cursorloc[1] += 1;
+            }
         }
-        if (key == GLFW_KEY_LEFT && handler->cursorloc[0] > 0)
+        if (key == GLFW_KEY_LEFT)
         {
-            handler->cursorloc[0] -= 1;
+            if (handler->cursorloc[0] > 0)
+            {
+                handler->cursorloc[0] -= 1;
+            }
+
+            else if (handler->cursorloc[0] == 0 && handler->cursorloc[1] > 0)
+            {
+                handler->cursorloc[0] = handler->textArr[handler->cursorloc[1] - 1].size();
+                handler->cursorloc[1] -= 1;
+            }
         }
         if (key == GLFW_KEY_UP && handler->cursorloc[1] > 0)
         {
+            if (handler->textArr[handler->cursorloc[1] - 1].size() < handler->cursorloc[0])
+            {
+                handler->cursorloc[0] = handler->textArr[handler->cursorloc[1] - 1].size();
+            }
             handler->cursorloc[1] -= 1;
         }
         if (key == GLFW_KEY_DOWN && handler->textArr[handler->cursorloc[1] + 1].size() > 0)
         {
+            if (handler->textArr[handler->cursorloc[1] + 1].size() < handler->cursorloc[0])
+            {
+                handler->cursorloc[0] = handler->textArr[handler->cursorloc[1] + 1].size();
+            }
             handler->cursorloc[1] += 1;
         }
         if (key == GLFW_KEY_ENTER)
         {
-            handler->textArr[handler->cursorloc[1]].insert(handler->cursorloc[0], 1, '\n');
             handler->cursorloc[0] = 0;
             handler->cursorloc[1] += 1;
         }
-        if (key == GLFW_KEY_BACKSPACE && handler->cursorloc[0] > 0)
+        if (key == GLFW_KEY_BACKSPACE)
         {
-            handler->textArr[handler->cursorloc[1]].erase(handler->cursorloc[0] - 1, 1);
-            handler->cursorloc[0] -= 1;
+            if (handler->cursorloc[0] == 0 && handler->cursorloc[1] > 0)
+            {
+                handler->cursorloc[0] = handler->textArr[handler->cursorloc[1] - 1].size();
+                handler->cursorloc[1] -= 1;
+            }
+            else if (handler->cursorloc[0] > 0)
+            {
+                handler->textArr[handler->cursorloc[1]].erase(handler->cursorloc[0] - 1, 1);
+                handler->cursorloc[0] -= 1;
+            }
         }
     }
     else
