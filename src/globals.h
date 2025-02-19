7ec730f73d54ad512c8058e993947cf7be415553
SJT00 <saadtaj96@gmail.com>
2025-02-18 23:30:53 -0500

    width never used

--- src/globals.h
+++ src/globals.h
@@ -20,7 +20,6 @@ namespace Globals
     constexpr ViewPort VIEWPORT = {.LEFT = PADDING, .RIGHT = SCR_WIDTH - PADDING, .TOP = PADDING, .BOTTOM = SCR_HEIGHT - PADDING, .WIDTH = SCR_WIDTH - 2 * PADDING, .HEIGHT = SCR_HEIGHT - 2 * PADDING};
     constexpr unsigned int LINESPACING = 6;
     constexpr unsigned int FONTSIZE = 14;
-    constexpr unsigned int FONTWIDTH = 8;
     constexpr const char *FONTSRC = "./assets/fonts/Futura.ttf";
 }
 
